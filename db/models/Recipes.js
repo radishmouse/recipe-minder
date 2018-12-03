bd6b3773b8f81d32c09ef2dc05b91418f53e5936
Chris Aquino <chris.aquino@gmail.com>
2018-12-03 17:45:24 -0500

    makes class singular, adds convenience method search by ingredient

--- db/models/Recipes.js
+++ db/models/Recipes.js
@@ -1,12 +1,16 @@
 const db = require('../conn');
 
-class Recipes {
+class Recipe {
   constructor(id, name, servings) {
     this.id = id;
     this.name = name;
     this.servings = servings;
   }
 
+  static convertMultiple(recipes) {
+    return recipes.map(r => new Recipe(r.id, r.name, r.servings));
+  }
+  
   static add(name, servings) {
     return db.one({
       name: 'add-recipe',
@@ -36,12 +40,32 @@ class Recipes {
         where
           name ilike '%$1:raw%'
       `, [searchText])   
-      .then(records => records.map(r => new Recipes(r.id, r.name, r.servings)));
+      .then(this.convertMultiple);
   }
 
-  static searchByServings() {
-    
+  static searchByServings(howMany, threshold=1) {
+    // Find a Recipe that provides =howMany= servings
+    return db.any(`
+      select * from recipes 
+        where 
+      servings > ($1 - $2) and servings < ($1 + $2)
+    `, [howMany, threshold]).then(this.convertMultiple);
+  }
+
+  static searchByIngredient(ingredientName){
+    return db.any(`
+      select * from recipes 
+        where 
+      id in (
+        select recipe_id from amounts 
+          where 
+        ingredient_id in (
+          select id from ingredients where name ilike '%$1:raw%'
+        )
+      )
+    `, [ingredientName])
+      .then(this.convertMultiple);
   }
 }
 
-module.exports = Recipes;
+module.exports = Recipe;
